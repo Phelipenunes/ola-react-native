9707eaa22f1f581597e652c4e9bab1181ef1d3f9
Phelipe <phelipesenac@gmail.com>
2024-02-07 10:32:28 -0300

    .
    .

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { View, Text, StatusBar } from "react-native";
+import {
+  View,
+  Text,
+  StatusBar,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
 export default function App() {
+  const vai = () => {
+    Alert.alert("Titulo da janela", "Fala parça!");
+  };
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <View>
-        <Text>Óla React Native</Text>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Text>Topo/Cabeçalho</Text>
+        </View>
+        <View>
+          <Text>Conteúdo</Text>
+        </View>
+        <View>
+          <Text>rodapé</Text>
+          <Button title="Tóca aqui!!!" onPress={vai} />
+        </View>
+      </SafeAreaView>
     </>
   );
 }
