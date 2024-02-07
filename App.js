514adc7d5c571d4e30cafd9337744b9b1b043061
Phelipe <phelipesenac@gmail.com>
2024-02-07 09:36:59 -0300

    status bar

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { View, Text, StatusBar } from "react-native";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Óla React Native 😎</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <View>
+        <Text>Óla React Native</Text>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
