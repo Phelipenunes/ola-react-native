011a69f48e5c54c745a10c99a283d305bc6413ca
Phelipe <phelipesenac@gmail.com>
2024-02-07 11:00:51 -0300

    css

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   SafeAreaView,
   Button,
   Alert,
+  StyleSheet,
 } from "react-native";
 export default function App() {
   const vai = () => {
@@ -13,14 +14,14 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <View>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.cabecalho}>
           <Text>Topo/Cabeçalho</Text>
         </View>
-        <View>
+        <View style={estilos.conteudo}>
           <Text>Conteúdo</Text>
         </View>
-        <View>
+        <View style={estilos.rodape}>
           <Text>rodapé</Text>
           <Button title="Tóca aqui!!!" onPress={vai} />
         </View>
@@ -28,3 +29,22 @@ export default function App() {
     </>
   );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "lightblue",
+    flex: 1,
+  },
+  cabecalho: {
+    backgroundColor: "red",
+    height: "10%",
+  },
+  conteudo: {
+    backgroundColor: "green",
+    height: "10%",
+  },
+  rodape: {
+    backgroundColor: "yellow",
+    height: "10%",
+  },
+});
